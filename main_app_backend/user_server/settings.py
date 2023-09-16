d4e079940d16feeacc53623aae340a2b42e333bd
João Machado <jtomaspm@ua.pt>
2023-09-16 19:17:13 +0100

    added settings to user server

---
+++ main_app_backend/user_server/settings.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+import logging
+
+load_dotenv(find_dotenv())
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    filename='user_server.log',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
