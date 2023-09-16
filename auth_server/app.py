ff477757ebab2e3758243621a1da87be7761b4fa
João Machado <jtomaspm@ua.pt>
2023-09-16 15:17:21 +0100

    added gitignore

--- auth_server/app.py
+++ auth_server/app.py
@@ -1,6 +1,7 @@
 from contextvars import Token
 from fastapi import FastAPI
-
+from auth_server.models.payload_data import PayloadData
+import time
 from services.redis_service import RedisService
 
 app = FastAPI()
@@ -8,5 +9,12 @@ redis_service = RedisService()
 
 @app.post("/validate/")
 async def validate_token(token: Token):
-
-    return {"message": "Hello World"}
+    token_data = redis_service.get_token(token.access_token)
+    if token_data.refresh_token == token.refresh_token:
+        if time.time() < token.expire_time_ms:
+            return {"status": "Valid",
+                    "payload": PayloadData(**token_data.__dict__),
+                    "token": token}
+        else:
+            return {"status": "Expired"}
+    return {"status": "Invalid"}
